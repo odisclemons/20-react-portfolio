69290c983f0bda4f677254dece4e8bd720aa43ff
Odis Clemons <odis.clemons@gmail.com>
2022-06-06 23:28:09 -0400

    added reset and base css

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import react from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1 class="hlt">Odis Clemons</h1>
+
+        <ul>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+      <div
+        class="jt"
+        role="img"
+        aria-label="Jumbotron image of laptop with code displayed"
+      >
+        <div class="green-cover"></div>
+        <div id="profile-pic" role="img" aria-label="profile picture of Odis">
+          &nbsp;
+        </div>
+        <h2 class="hlt">dreadHeadDev welcomes you!</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
